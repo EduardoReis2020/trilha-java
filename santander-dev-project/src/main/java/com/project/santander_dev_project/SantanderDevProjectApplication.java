61db32b46810b2b775a8e1d5efb9fcb4d51941e1
EduardoReis2020 <eduardodosreissouza@gmail.com>
2025-06-03 22:42:17 -0300

    up

---
+++ santander-dev-project/src/main/java/com/project/santander_dev_project/SantanderDevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.santander_dev_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevProjectApplication.class, args);
+	}
+
+}
---
+++ santander-dev-project/src/test/java/com/project/santander_dev_project/SantanderDevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.santander_dev_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
